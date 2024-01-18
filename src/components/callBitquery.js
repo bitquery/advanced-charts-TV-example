1828f1cf017bbcdecc53c9c761f76499a9423d59
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2024-01-18 20:50:10 +0530

    query updated for time range

--- src/components/callBitquery.js
+++ src/components/callBitquery.js
@@ -14,7 +14,9 @@ export const TOKEN_DETAILS = `
       orderBy: {ascending: Block_Time}
       where: {Trade: {Currency: {SmartContract: {is: "0xdac17f958d2ee523a2206206994597c13d831ec7"}}, 
       Side: {Currency: {SmartContract: {is: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"}},Type: {is: buy}}
+  
     }
+    Block:{Time:{since:"2023-11-05T00:00:40Z"}}
   
   }
       limit: {count: 15000}
@@ -38,5 +40,3 @@ export const TOKEN_DETAILS = `
 }
 
   `;
-
-
