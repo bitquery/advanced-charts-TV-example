40a86bb46299fd79d3a68eaf9207d8e8396cfdd8
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2024-10-09 18:41:38 +0530

    Update callBitquery.js

--- src/components/callBitquery.js
+++ src/components/callBitquery.js
@@ -13,28 +13,25 @@ export const TOKEN_DETAILS = `
     DEXTradeByTokens(
       orderBy: {ascendingByField: "Block_OHLC_interval"}
       where: {Trade: {Currency: {SmartContract: {is: "0xdac17f958d2ee523a2206206994597c13d831ec7"}}, 
-      Side: {Currency: {SmartContract: {is: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"}}}
-    }
-      Block:{Time:{since:"2023-12-05T00:00:40Z", till:"2024-01-05T00:00:40Z"}}
-  
-  }
+      Side: {Currency: {SmartContract: {is: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"}}}},
+      Block: {Time: {since: "2023-12-05T00:00:40Z", till: "2024-01-05T00:00:40Z"}}}
       limit: {count: 15000}
     ) {
-    Block {
-         OHLC_interval: Time(interval: {in: minutes, count:1})
+      Block {
+        OHLC_interval: Time(interval: {in: minutes, count: 1})
       }
-     
       volume: sum(of: Trade_Amount)
       Trade {
-        high: Price(maximum: Trade_Price, selectWhere:{lt:20000})
-        low: Price(minimum: Trade_Price, selectWhere:{lt:20000})
-        open: Price(minimum: Block_Number, selectWhere:{lt:20000})
-        close: Price(maximum: Block_Number, selectWhere:{lt:20000})
+        high: Price(maximum: Trade_Price)
+        low: Price(minimum: Trade_Price)
+        open: Price(minimum: Block_Number)
+        close: Price(maximum: Block_Number)
       }
       count
     }
   }
 }
+
 
 
   `;
